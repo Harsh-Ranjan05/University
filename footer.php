a47bf25c82af085f18ed67b76d89179b1442b488
Harsh-Ranjan05 <harshranjan1644@gmail.com>
2025-08-12 22:46:42 +0530

    Added Header And Footer From External Page

---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer class="text-white pt-5 pb-3" style="background-color:#125A33;">
+  <div class="container">
+    <div class="row">
+      <!-- About -->
+      <div class="col-md-4 mb-4">
+        <h5>About BGU</h5>
+        <p>BHARTI GYANPEETH UNIVERSITY (BGU) is committed to excellence in education, innovation, and holistic development of students across various disciplines.</p>
+      </div>
+
+      <!-- Quick Links -->
+      <div class="col-md-4 mb-4">
+        <h5>Quick Links</h5>
+        <ul class="list-unstyled">
+          <li><a href="index.php" class="text-white text-decoration-none">Home</a></li>
+          <li><a href="admissions.php" class="text-white text-decoration-none">Admissions</a></li>
+          <li><a href="programs.php" class="text-white text-decoration-none">Programs</a></li>
+          <li><a href="contact.php" class="text-white text-decoration-none">Contact Us</a></li>
+        </ul>
+      </div>
+
+      <!-- Contact -->
+      <div class="col-md-4 mb-4">
+        <h5>Contact</h5>
+        <p><i class="fas fa-map-marker-alt me-2"></i>BHARTI GYANPEETH UNIVERSITY campus,Ranchi,Tatisilwai,Jharkhand 835103</p>
+        <p><i class="fas fa-phone me-2"></i>+91 98765 43210</p>
+        <p><i class="fas fa-envelope me-2"></i>info@bgu.edu.in</p>
+        <div class="d-flex gap-2 mt-3">
+          <a href="#" class="text-white"><i class="fab fa-facebook fa-lg"></i></a>
+          <a href="#" class="text-white"><i class="fab fa-twitter fa-lg"></i></a>
+          <a href="#" class="text-white"><i class="fab fa-linkedin fa-lg"></i></a>
+          <a href="#" class="text-white"><i class="fab fa-instagram fa-lg"></i></a>
+        </div>
+      </div>
+    </div>
+
+    <hr class="border-light" />
+    <div class="text-center">
+      <small>&copy; <?php echo date("Y"); ?> BHARTI GYANPEETH UNIVERSITY. All rights reserved.</small>
+    </div>
+  </div>
+</footer>
